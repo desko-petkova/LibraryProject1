36d6e9e4b68f6f60e55d8660b6aeed834c5dee40
desko-petkova <desko.petkova@gmail.com>
2025-06-27 12:17:39 +0300

    Writer and Reader classes added

---
+++ Data/Writer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject1.Data
+{
+    public class Writer
+    {
+    }
+}
